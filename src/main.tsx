659ae5069fa096c7d1ac4aed712a1d36f59e11fb
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-08 17:11:56 -0500

    added footer to layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Header from './components/header.tsx'
+import Footer from './components/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
     <App />
+    <Footer />
   </StrictMode>,
 )
