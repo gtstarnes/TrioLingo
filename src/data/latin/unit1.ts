199bacea06ef02d3161206a6671f04a8c4959679
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-08 16:58:58 -0500

    created unit one for latin

---
+++ src/data/latin/unit1.ts
@@ -0,0 +1,79 @@
+const unit1 = [
+    {
+        phrase: 'Salve',
+        answer: 'Hello',
+        score: 0,
+    },
+    {
+        phrase: 'Bonum mane',
+        answer: 'Good morning',
+        score: 0,
+    },
+    {
+        phrase: 'Bonum nocte',
+        answer: 'Good night',
+        score: 0,
+    },
+    {
+        phrase: 'Vale',
+        answer: 'goodbye',
+        score: 0,
+    },
+    {
+        phrase: 'Quaeso',
+        answer: 'Please',
+        score: 0,
+    },
+    {
+        phrase: 'Gratias tibi',
+        answer: 'Thank you',
+        score: 0,
+    },
+    {
+        phrase: 'Nihil est',
+        answer: 'You are welcome',
+        score: 0,
+    },
+    {
+        phrase: 'Ita',
+        answer: 'yes',
+        score: 0,
+    },
+    {
+        phrase: 'Non',
+        answer: 'no',
+        score: 0,
+    },
+    {
+        phrase: 'Ignosce mihi',
+        answer: 'excuse me',
+        score: 0,
+    },
+    {
+        phrase: 'Me paenitet',
+        answer: 'I am sorry',
+        score: 0,
+    },
+    {
+        phrase: 'Quomodo vales',
+        answer: 'how are you',
+        score: 0,
+    },
+    {
+        phrase: 'Bene, gratias',
+        answer: 'I am fine, thanks',
+        score: 0,
+    },
+    {
+        phrase: 'Quid est nomen tibi',
+        answer: 'what is your name',
+        score: 0,
+    },
+    {
+        phrase: 'Te convenire iuvat',
+        answer: 'nice to meet you',
+        score: 0,
+    },
+]
+
+export default unit1;
