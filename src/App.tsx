d02dd7d81541c7d3049759efa381cba7c7a97b7f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-08 17:23:22 -0500

    created basic display for unit 1 words

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 
 import './App.css'
+import unit1 from './data/latin/unit1'
 
 function App() {
+
+  const lang = unit1;
 
 
   return (
     <>
       <h1>Welcome!</h1>
       <h3>Please Pick a Language:</h3>
+      <h4>Test</h4>
+
+      <div>
+        {lang.map(phrase => {
+          return (
+            <div>{phrase.phrase}</div>
+          )
+        })}
+      </div>
     </>
   )
 }
